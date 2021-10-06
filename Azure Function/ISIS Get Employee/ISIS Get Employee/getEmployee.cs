76048648435c56dcfe2cc2843bad737fd9a2d31b
JuniorMejia2 <jrmejiaval42@tntech.edu>
2021-10-06 09:21:32 -0500

    added resources

--- Azure Function/ISIS Get Employee/ISIS Get Employee/getEmployee.cs
+++ Azure Function/ISIS Get Employee/ISIS Get Employee/getEmployee.cs
@@ -13,20 +13,37 @@ namespace ISISGetEmployee
 
     public static class getEmployee
     {
+        private class Agency
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+            public Agency (string strAgencyName, string strAddress, string strPhone) 
+            {
+                Name = strAgencyName;
+                Address = strAddress;
+                Phone = strPhone;
+            }
+        }
         private class Employee
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string CodeName { get; set; }
             public string Position { get; set; }
-            public string Status { get; }
-            public Employee(string strFirstName, string strLastName, string strCodeName, string strPosition, string strStatus)
+            public string Status { get; set; }
+            public double WeeklyPay { get; set; }
+
+            public Agency Agency { get; set; }
+            public Employee(string strFirstName, string strLastName, string strCodeName, string strPosition, string strStatus, double dblPayrate, double dblHours, string strAgency, Agency agUSAgency)
             {
                 FirstName = strFirstName;
                 LastName = strLastName;
                 CodeName = strCodeName;
                 Position = strPosition;
                 Status = strStatus;
+                WeeklyPay = dblPayrate * dblHours;
+                Agency = agUSAgency;
             }
         }
 
@@ -36,39 +53,38 @@ namespace ISISGetEmployee
             ILogger log)
         {
             string strCodeName = req.Query["CodeName"];
+            string strAGency = req.Query["Agency"];
             log.LogInformation("HTTP trigger on getEmployee processed a request for: " + strCodeName);
 
+            Agency ISIS = new Agency ("ISIS", "10 E Broad st", "931-526-2125");
+            Agency CIA = new Agency("CIA", "10 E Borad st", "931-526-2125");
 
-
-            Employee Archer = new Employee("Sterling", "Archer", "Duchess", "Field Agent", "Active");
-            Employee Lana = new Employee("Lana", "Kane", "Truckasaurus", "Field Agent", "Active");
-
+            Employee Archer = new Employee("Sterling", "Archer", "Duchess", "Field Agent", "Active", 23.75, 18.50, "ISIS",ISIS);
+            Employee Lana = new Employee("Lana", "Kane", "Truckasaurus", "Field Agent", "Active", 21.50, 23.5, "ISIS", ISIS);
+            Employee Pam = new Employee("Pam", "Poovey", "Snowball", "Hunam Resoruces", "Active", 49.00, 12, "ISIS", ISIS);
+            Employee Barry = new Employee("Barry", "Cyborg", "Duchess", "Field Agent ", "Active", 23.75, 18.50, "CIA", CIA);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            if (strCodeName == null)
+            List<Employee> arrEmployees = new Employee[] { Archer, Lana, Pam, Barry };
+
+            list<Employee> 1stFoundEmployee = new List <Employee>();
+            foreach (Employee empCurrent in arrEmployees)
             {
-                return new OkObjectResult("Employee Not Found");
+                if (strCodeName == empCurrent.CodeName)
+                {
+                    1stFoundEmployee.Add(empCurrent);
+                }
+            }
+            if(1stEMployeeFound.Count > 0)
+            {
+                return new OkObjectResult(1stFoundEMployee);
             }
             else
             {
-                if (strCodeName == "Duchess")
-                {
-                    return new OkObjectResult(Archer);
-                }
-                else if (strCodeName == "Truckasaurus")
-                {
-                    return new OkObjectResult(Lana);
-                }
-                else
-                {
-                    return new OkObjectResult("Employee Not Found");
-                }
-
+                return new OkObjectResult("Employee Not Found");
             }
-
-
         }
     }
 }
